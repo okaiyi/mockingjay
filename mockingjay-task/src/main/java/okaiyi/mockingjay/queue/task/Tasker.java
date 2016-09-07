57581a5722dc1a14c5d60b02ff3acfb9b9a0788f
Administrator <Administrator@kai-computer>
2016-09-07 09:47:48 +0800

    d

--- mockingjay-task/src/main/java/okaiyi/mockingjay/queue/task/Tasker.java
+++ mockingjay-task/src/main/java/okaiyi/mockingjay/queue/task/Tasker.java
@@ -1,12 +1,16 @@
 package okaiyi.mockingjay.queue.task;
+
+import java.io.Serializable;
+
 /**
  * 线程任务,泛型T用来指定无法被持久化或者保存的内容,例如Spring所管理的service或者bean
+ * T 无法被持久化的东西
  */
-public interface Tasker{
+public interface Tasker<T> extends Serializable{
 	/**
 	 * 执行任务,并返回任务执行结果
 	 */
-	TaskResult doTask();
+	TaskResult doTask(T t);
 	/**
 	 * 获取该任务执行的唯一ID，同一操作的任务,ID必须一致
 	 * @return
